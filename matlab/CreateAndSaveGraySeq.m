fbd384f30914ff3df4ee9a670f3e896c48ea3224
realingy <754037927@qq.com>
2019-12-05 09:27:03 +0800

    gray码pattern生成matlab程序

---
+++ matlab/CreateAndSaveGraySeq.m
@@ -0,0 +1,14 @@
+% 使用方法：修改分辨率resolution，再运行即可
+
+% resolution = [1024, 768];
+resolution = [1024, 720];
+  
+graySeq = GrayCodePattern4ProjectorCalibration(resolution);
+% graySeq = GrayCodePattern(resolution);
+
+s = size(graySeq);
+
+for i = 1:s(3)
+    imwrite(graySeq(:,:,i), sprintf('./output/%d.jpg', i));
+end
+
